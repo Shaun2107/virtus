2c28f4aa5183bd0615c88bbf7f78dc00d1466da4
virtus <99126085+virtus2022@users.noreply.github.com>
2022-02-06 21:28:35 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,88 @@
+/*map slideout*/
+var a;
+function show_hide() {
+    if(a==1) {
+        document.getElementById("mySidenav").style.width = "620px";
+        return a=0;
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "0";
+        return a=1;
+    }
+}
+
+/*map stuff*/
+$(function() {
+ 
+    $('#us2').locationpicker({
+       location: {latitude: 25.2048493, longitude: 55.2707828},   
+       radius: 0,
+       inputBinding: {
+          latitudeInput: $('#lat'),
+          longitudeInput: $('#lng'),
+          locationNameInput: $('#location1')
+       },
+       enableAutocomplete: true,
+    });
+     
+     
+});
+
+/*get values from prev page*/
+function passvalues() {
+    var pickup = document.getElementById("location1").value;
+    var dropoff = document.getElementById("location2").value;
+    localStorage.setItem("pickup", pickup);
+    localStorage.setItem("dropoff", dropoff);
+    return false;
+}
+
+/*price setter*/
+function price1() {
+    document.getElementById("price").innerText = "Price: 25.00 AED";
+    localStorage.setItem("vehicle", "SUV");
+}
+
+function price2() {
+    document.getElementById("price").innerText = "Price: 30.50 AED";
+    localStorage.setItem("vehicle", "VAN");
+}
+
+/*accordion*/
+var coll = document.getElementsByClassName("card");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = '500px';
+    } 
+  });
+}
+
+
+/*login checker*/
+function loginchecker() {
+    if (localStorage.getItem("loginkey") == "1") {
+      document.getElementById("in").innerText = "Sign Out";
+    }
+}
+
+function signout() {
+    if (document.getElementById("in").innerText == "Sign Out") {
+      document.getElementById("in").href = "index.html";
+      localStorage.removeItem("loginkey");
+    }
+}
+
+
+function logincheckerbook() {
+    if (localStorage.getItem("loginkey") == "1") {
+      document.getElementById("submit-btn3").href = "booked.html";
+      document.getElementById("submit-btn4").href = "booked.html";
+    }
+  }
